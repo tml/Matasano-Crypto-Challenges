da6282ff308e624ed1bddc3c8e1ead4d931e6073
Tom Deakin <thomasdeakin@gmail.com>
2014-09-06 00:04:25 +0100

    Convert indentation to spaces

--- crypto.py
+++ crypto.py
@@ -13,104 +13,104 @@ from Crypto.Cipher import AES
 
 # Convert a hex string to bytes
 def hex_to_bytes(hexstr):
-	return bytes.fromhex(hexstr)
+    return bytes.fromhex(hexstr)
 
 # Convert bytes to ascii string of base64 characters
 def bytes_to_base64(b):
-	return base64.b64encode(b).decode()
+    return base64.b64encode(b).decode()
 
 # Convert base64 string to bytes
 def base64_to_bytes(b):
-	return base64.b64decode(b)
+    return base64.b64decode(b)
 
 # Convert hex string to base64 ascii string
 def hex_to_base64(hexstr):
-	return bytes_to_base64(hex_to_bytes(hexstr))
+    return bytes_to_base64(hex_to_bytes(hexstr))
 
 # Convert bytes to a hex string
 def bytes_to_hex(b):
-	return binascii.b2a_hex(b).decode()
+    return binascii.b2a_hex(b).decode()
 
 # Convert ascii string to bytes
 def str_to_bytes(string):
-	return bytes(string, encoding='utf-8')
+    return bytes(string, encoding='utf-8')
 
 # Cyphers
 
 # XOR key with plaintext
 def fixed_xor(plain, key):
-	return bytes([x ^ y for x,y in zip(plain, cycle(key))])
+    return bytes([x ^ y for x,y in zip(plain, cycle(key))])
 
 
 def brute_xor(ciphertext, keys):
-	'''
-	Brute force a plaintext which has been encrypted with an XOR cipher
-	by trying all the keys in the list. The plaintext which looks most
-	like a piece of english text will be the winner.
+    '''
+    Brute force a plaintext which has been encrypted with an XOR cipher
+    by trying all the keys in the list. The plaintext which looks most
+    like a piece of english text will be the winner.
 
-	Returns the best key and the plaintext
-	'''
-	best = (0.0, None, None)
-	for key in keys:
-		try:
-			plain = fixed_xor(ciphertext, key).decode()
-			score = textutils.probability_english(plain)
-			if score > best[0] and all(c in string.printable for c in plain):
-				best = (score, key, plain)
-		except UnicodeDecodeError:
-			pass
-	return best[1:]
+    Returns the best key and the plaintext
+    '''
+    best = (0.0, None, None)
+    for key in keys:
+        try:
+            plain = fixed_xor(ciphertext, key).decode()
+            score = textutils.probability_english(plain)
+            if score > best[0] and all(c in string.printable for c in plain):
+                best = (score, key, plain)
+        except UnicodeDecodeError:
+            pass
+    return best[1:]
 
 
 def brute_xor_keysize(cipher):
-	'''
-	Take different key sizes and find the average normalised (by key length) hamming distance
-	between all pairs of the first 4 blocks of ciphertext. The lowest wins.
-	'''
-	sizes = range(2, 40)
-	keysizes = {}
-	for size in sizes:
-		# Take 4 blocks of length key
-		blocks = [cipher[i*size:size*(i+1)] for i in range(0, 4)]
-		assert all(len(block) == size for block in blocks)
-		# Compute hamming distances
-		distances = list(starmap(textutils.hamming_distance, product(blocks, blocks[1:])))
-		# Compute normalised average
-		#average = float(sum(distances))/float(len(distances))/float(size)
-		average = sum(float(d)/float(size) for d in distances) / float(len(distances))
-		keysizes[size] = average
-	
-	# Return the smallest
-	print(keysizes)
-	return min(keysizes, key=keysizes.get)
+    '''
+    Take different key sizes and find the average normalised (by key length) hamming distance
+    between all pairs of the first 4 blocks of ciphertext. The lowest wins.
+    '''
+    sizes = range(2, 40)
+    keysizes = {}
+    for size in sizes:
+        # Take 4 blocks of length key
+        blocks = [cipher[i*size:size*(i+1)] for i in range(0, 4)]
+        assert all(len(block) == size for block in blocks)
+        # Compute hamming distances
+        distances = list(starmap(textutils.hamming_distance, product(blocks, blocks[1:])))
+        # Compute normalised average
+        #average = float(sum(distances))/float(len(distances))/float(size)
+        average = sum(float(d)/float(size) for d in distances) / float(len(distances))
+        keysizes[size] = average
+    
+    # Return the smallest
+    print(keysizes)
+    return min(keysizes, key=keysizes.get)
 
 def brute_xor_key(cipher, keysize):
-	'''
-	Given a keysize, split the cipher into blocks of length keysize, then transpose
-	and concatenate the blocks. Do single letter XOR on each block to find the key.
-	'''
-	blocks = [cipher[i::keysize] for i in range(keysize)]
-	assert len(cipher) == sum(len(b) for b in blocks)
-	key = []
-	for block in blocks:
-		keys = [[b] for b in range(0, 255)]
-		k, _ = brute_xor(block, keys)
-		key.append(k[0])
-	return ''.join(map(chr,key))
+    '''
+    Given a keysize, split the cipher into blocks of length keysize, then transpose
+    and concatenate the blocks. Do single letter XOR on each block to find the key.
+    '''
+    blocks = [cipher[i::keysize] for i in range(keysize)]
+    assert len(cipher) == sum(len(b) for b in blocks)
+    key = []
+    for block in blocks:
+        keys = [[b] for b in range(0, 255)]
+        k, _ = brute_xor(block, keys)
+        key.append(k[0])
+    return ''.join(map(chr,key))
 
 
 def decode_AES_ECB(cipher, key):
-	obj = AES.new(key, AES.MODE_ECB)
-	return obj.decrypt(cipher)
+    obj = AES.new(key, AES.MODE_ECB)
+    return obj.decrypt(cipher)
 
 def repeated_block(text):
-	'''
-	Detect ECB mode. If the text has a repeated block, it means
-	that the plaintext was likely encoded using ECB mode.
-	'''
-	block_length = 16
-	blocks = [text[i*block_length:block_length*(i+1)] for i in range(int(len(text)/block_length))]
-	if len(set(blocks)) != len(blocks):
-		return True
-	return False
+    '''
+    Detect ECB mode. If the text has a repeated block, it means
+    that the plaintext was likely encoded using ECB mode.
+    '''
+    block_length = 16
+    blocks = [text[i*block_length:block_length*(i+1)] for i in range(int(len(text)/block_length))]
+    if len(set(blocks)) != len(blocks):
+        return True
+    return False
 
--- textutils.py
+++ textutils.py
@@ -3,69 +3,69 @@ import math
 
 # From http://www.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
 ENGLISH_FREQUENCIES = {
-	'E' : .1202,
-	'T' : .0910,
-	'A' : .0812,
-	'O' : .0768,
-	'I' : .0731,
-	'N' : .0695,
-	'S' : .0628,
-	'R' : .0602,
-	'H' : .0592,
-	'D' : .0432,
-	'L' : .0398,
-	'U' : .0288,
-	'C' : .0271,
-	'M' : .0261,
-	'F' : .0230,
-	'Y' : .0211,
-	'W' : .0209,
-	'G' : .0203,
-	'P' : .0182,
-	'B' : .0149,
-	'V' : .0111,
-	'K' : .0069,
-	'X' : .0017,
-	'Q' : .0011,
-	'J' : .0010,
-	'Z' : .0007
+    'E' : .1202,
+    'T' : .0910,
+    'A' : .0812,
+    'O' : .0768,
+    'I' : .0731,
+    'N' : .0695,
+    'S' : .0628,
+    'R' : .0602,
+    'H' : .0592,
+    'D' : .0432,
+    'L' : .0398,
+    'U' : .0288,
+    'C' : .0271,
+    'M' : .0261,
+    'F' : .0230,
+    'Y' : .0211,
+    'W' : .0209,
+    'G' : .0203,
+    'P' : .0182,
+    'B' : .0149,
+    'V' : .0111,
+    'K' : .0069,
+    'X' : .0017,
+    'Q' : .0011,
+    'J' : .0010,
+    'Z' : .0007
 }
 
 def probability_english(text):
-	'''
-	Use the Bhattacharyya coefficient to determine if the text is likely to be English.
-	Higher is better.
+    '''
+    Use the Bhattacharyya coefficient to determine if the text is likely to be English.
+    Higher is better.
 
-	Whitespace is ignored.
-	'''
+    Whitespace is ignored.
+    '''
 
-	text = text.upper()
-	frequencies = {}
-	for letter in text:
-		if letter in frequencies:
-			frequencies[letter] += 1.
-		else:
-			frequencies[letter] = 1.
+    text = text.upper()
+    frequencies = {}
+    for letter in text:
+        if letter in frequencies:
+            frequencies[letter] += 1.
+        else:
+            frequencies[letter] = 1.
 
-	total = sum(frequencies.values())
-	for letter in frequencies.keys():
-		frequencies[letter] /= total
+    total = sum(frequencies.values())
+    for letter in frequencies.keys():
+        frequencies[letter] /= total
 
-	score = 0.0
-	for l in ENGLISH_FREQUENCIES.keys():
-		if l not in frequencies:
-			frequencies[l] = 0.0
-		score += math.sqrt(frequencies[l] * ENGLISH_FREQUENCIES[l])
+    score = 0.0
+    for l in ENGLISH_FREQUENCIES.keys():
+        if l not in frequencies:
+            frequencies[l] = 0.0
+        score += math.sqrt(frequencies[l] * ENGLISH_FREQUENCIES[l])
 
-	return score
+    return score
 
 def hamming_distance(s1, s2):
-	'''
-	Compute the edit/hamming distance of two _byte_ strings.
-	This is done by XOR'ing each byte, and then converting the number
-	to a binary string in the form '0b00..'. The leading 0b is stripped
-	and the 1's counted.
-	'''
-	if len(s1) != len(s2):
-		raise ValueError('Hamming Distance: strings must be of equal length')
-	return sum(bin(c1 ^ c2)[2:].count('1') for c1,c2 in zip(s1, s2))
+    '''
+    Compute the edit/hamming distance of two _byte_ strings.
+    This is done by XOR'ing each byte, and then converting the number
+    to a binary string in the form '0b00..'. The leading 0b is stripped
+    and the 1's counted.
+    '''
+    if len(s1) != len(s2):
+        raise ValueError('Hamming Distance: strings must be of equal length')
+    return sum(bin(c1 ^ c2)[2:].count('1') for c1,c2 in zip(s1, s2))
